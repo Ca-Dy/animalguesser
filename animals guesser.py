d25e7b6699e003b351ff1af2ab2e2c303a38c89d
Ca-Dy <69281371+Ca-Dy@users.noreply.github.com>
2020-08-06 14:29:25 +1100

    Add files via upload

--- animals guesser.py
+++ animals guesser.py
@@ -1,96 +1,49 @@
-#Devineur d'animaux
-import webbrowser
+#Devineur d'animaux entre chat, chien et perroquet
+#CABELLO Dylan 
+try:
+    import webbrowser
+except ModuleNotFoundError:
+    print("Impossible d'importer le module webbrowser")
+    webbrowser = None
 #Règles
 print("les règles du jeu sont simples: y = oui | n = non")
 def choix_(a):
     b = input(a)
-    if b == "y":
-        return True
-    else:
-        return False
+    return b == "y"
+
 def fin():
     print("merci d'avoir joué")
 #Fonctions
 def poilu():
-    if choix_("ton animal a t-il des poils?  ") == True:
-        return True
-    else:
-        return False
+    return bool(choix_("ton animal a t-il des poils?  "))
 
 def os():
-    if choix_("ton animal préfère t-il les os?  ") == True:
-        return True
-    else:
-        return False
+    return bool(choix_("ton animal préfère t-il les os?  "))
 
 def poisson():
-    if choix_("ton animal aime t-il le lait et le poisson?  ") == True:
-        return True
-    else:
-        return False
+    return bool(choix_("ton animal aime t-il le lait et le poisson?  "))
 
 def plumes():
-    if choix_("ton animal a t-il des plumes?  ") == True:
-        return True
-    else:
-        return False
-
+    return bool(choix_("ton animal a t-il des plumes?  "))
 def parrot():
-    if choix_("ton animal répète-il ce que tu dis?  ") == True:
-        return True
-    else:
-        return False
+    return bool(choix_("ton animal répète-il ce que tu dis?  ")) 
 
 #MainLoop
-if poilu() == True:
-    if os() == True:
+if poilu():
+    if os():
         print("ton animal est un chien!")
-        if choix_("veux tu savoir ce qu'est un chien?  ") == True:
-            try:
-                webbrowser.open("https://fr.wikipedia.org/wiki/Chien")
-                fin()
-                quit()
-            except:
-                print("le module webbrowser n'est pas installé -_-")
-                fin()
-                quit()
-        else:
-            fin()
-            quit()
-    elif poisson() == True:
+        if webbrowser and choix_("veux tu savoir ce qu'est un chien?  "):
+            webbrowser.open("https://fr.wikipedia.org/wiki/Chien")
+    elif poisson():
         print("ton animal est un chat")
-        if choix_("veux tu savoir ce qu'est un chat?  ") == True:
-            try:
-                webbrowser.open("https://fr.wikipedia.org/wiki/Chat")
-                fin()
-                quit()
-            except:
-                print("le module webbrowser n'est pas installé -_-")
-                fin()
-                quit()
-        else:
-            fin()
-            quit()
-elif plumes() == True:
-    if parrot() == True:
+        if webbrowser and choix_("veux tu savoir ce qu'est un chat?  "):
+            webbrowser.open("https://fr.wikipedia.org/wiki/Chat")
+elif plumes():
+    if parrot():
         print("ton animal est un perroquet")
-        if choix_("veux tu savoir ce qu'est un perroquet?  ") == True:
-            try:
-                webbrowser.open("https://fr.wikipedia.org/wiki/Perroquet")
-                fin()
-                quit()
-            except:
-                print("le module webbrowser n'est pas installé -_-")
-                fin()
-                quit()
-        else:
-            fin()
-            quit()
-    else:
-        print("je n'ai pas trouvé")
-        fin()
-        quit()
+        if webbrowser and choix_("veux tu savoir ce qu'est un perroquet?  "):
+            webbrowser.open("https://fr.wikipedia.org/wiki/Perroquet")
 else:
-        print("je n'ai pas trouvé")
-        fin()
-        quit()
+    print("je n'ai pas trouvé")
+fin()
+quit()
